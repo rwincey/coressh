89c1b67cb0591c5a34faf517e161acbb51c7437c
rwincey <winceyr@gmail.com>
2021-01-15 16:31:09 -0500

    Update core_install.sh

--- core_install.sh
+++ core_install.sh
@@ -34,6 +34,9 @@
 # Install openssh and ezremaster
 tce-load -iw openssh.tcz ezremaster.tcz nmap.tcz nmap-dbs.tcz tcpdump.tcz python.tcz socat.tcz curl.tcz proxychains-ng.tcz filesystems-5.4.3-tinycore.tcz        
            
+#Copy the sshd config file
+sudo cp /usr/local/etc/ssh/sshd_config.orig /usr/local/etc/ssh/sshd_config
+
 # Start the SSH server
 sudo /usr/local/etc/init.d/openssh start
 
